a71806eebd33e1610af562921a2c03c751086cbf
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-07-23 18:22:47 +0330

    responsive-welComponent

--- my-project/src/component/welcomeBanner.jsx
+++ my-project/src/component/welcomeBanner.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Logo from '../assets/image/illustration-working.svg'
 export default function welcomeBanner() {
     return (
-        <div className='flex lg:flex-row   sm:justify-center sm:items-center md:flex-col-reverse sm:flex-col-reverse  py-[4%]'>
-
-            <div className='flex flex-col  w-[50%] lg:pl-[10%] sm:items-center md:items-baseline  sm:p-0 gap-[1%] text-center'>
-                <h1 className='font-bold lg:text-[70px]  text-[#3e3e3e;] sm:text-[38px]'>More than just shorter links</h1>
-                <p className='text-gray-400 lg:text-[23px] sm:text-[18px] ' >Build your brand's recognition and get detailed insights on how your links are performing.</p>
-                <button className='text-white font-bold bg-[#2acfcf] w-[34%] py-[10px] rounded-[20px] mt-[3%]'>Get Start</button>
+        <div className='flex lg:flex-row container justify-center items-center mobile:flex-col-reverse md:flex-col-reverse sm:flex-col-reverse  py-12'>
+            <div className='flex flex-col md:mt-8 lg:mt-0 sm:mt-8 mobile:mt-8  items-center text-center' >
+                <h1   className='font-bold lg:text-7xl lg:leading-tight md:text-5xl text-[#3e3e3e;] sm:text-4xl mobile:text-2xl'>More than just shorter links</h1>
+                <p className='text-gray-400 lg:text-xl sm:text-lg lg:px-[13%] lg:leading-normal ' >Build your brand's recognition and get detailed insights on how your links are performing.</p>
+                <button className='text-white font-bold bg-[#2acfcf]  py-[10px] px-16 rounded-[20px] mt-9'>Get Start</button>
             </div>
-            <div className='sm:w-[50%] lg:w-[50%] md:w-[90%] lg:p-0 md:p-0 md:pl-[3%]'>
+            <div>
                 <img src={Logo} alt="" />
             </div>
         </div>
