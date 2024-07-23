ca26709cf92c366863b15ff7043f1c106ab28c2d
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-07-24 00:01:02 +0330

    Link responsive

--- my-project/src/component/LinkShorten.jsx
+++ my-project/src/component/LinkShorten.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function LinkShorten() {
     return (
-        <div className='flex justify-center '>
-            <div className='form-link  absolute bg-[#3b3054]  sm:w-[55%]  lg:w-[82%] p-[4%] rounded-[11px] '>
-                <form className='flex lg:flex-row sm:flex-col items-center sm:gap-[7px]' >
-                    <input className='py-[2%] lg:px-[30%] sm:px-[20%]  rounded-[4px] ' type="text" placeholder='Shorten a link here...' />
-                    <button type='submit' className='text-white ml-[3%]  sm:w-[100%] font-bold py-[2%] rounded-[7px] bg-[#2acfcf] w-[16%]  ' >Shorten it!</button>
+        <div className='container absolute flex items-center justify-center '>
+            <div className='grid w-[82%] bg-[#3b3054] rounded-xl '>
+                <form className=' grid lg:grid-cols-6  mobile:gap-2 mobile:p-3 md:p-5 sm:p-4 lg:px-10 lg:py-12   md:grid-cols-1 sm:grid-cols-1 items-center justify-center sm:gap-[7px] ' >
+                    <input className='rounded-md lg:text-lg md:text-base lg:py-4 lg:col-span-5 px-72 sm:px-0 sm:text-center mobile:px-8 mobile:py-2 mobile:text-sm ' type="text" placeholder='Shorten a link here...' />
+                    <button type='submit' className='text-white  font-bold lg:py-4  rounded-[7px] bg-[#2acfcf] mobile:py-2 ' >Shorten it!</button>
                 </form>
             </div>
         </div>
