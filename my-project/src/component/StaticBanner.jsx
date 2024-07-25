eb4f801fd0781e20690ee4c9684e79febab034ac
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-07-25 12:01:30 +0330

    done debug

--- my-project/src/component/StaticBanner.jsx
+++ my-project/src/component/StaticBanner.jsx
@@ -1,48 +1,64 @@
-import React from 'react'
-import record from '../assets/image/icon-detailed-records.svg'
-import customize from '../assets/image/icon-fully-customizable.svg'
-import recognation from '../assets/image/icon-brand-recognition.svg'
+import React from "react";
+import record from "../assets/image/icon-detailed-records.svg";
+import customize from "../assets/image/icon-fully-customizable.svg";
+import recognation from "../assets/image/icon-brand-recognition.svg";
 export default function StaticBanner() {
-    return (
-        <div className='bg-[#eff0f5]  lg:mt-20 md:mt-16  sm:mt-14 mobile:mt-12'>
-
-            <div className='flex flex-col items-center justify-center pt-32 pb-20 text-center '>
-
-                <h2 className='text-[#35323e] font-bold lg:text-5xl sm:text-3xl '>Advanced Statistics</h2>
-                <p className='py-2 text-gray-500 lg:text-lg sm:text-base mobile:text-sm'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+  return (
+    <div className="bg-[#eff0f5]  lg:mt-20 md:mt-16  sm:mt-14 mobile:mt-12">
+      <div className="flex flex-col items-center justify-center pt-32 pb-20 text-center ">
+        <h2 className="text-[#35323e] font-bold lg:text-5xl sm:text-3xl ">
+          Advanced Statistics
+        </h2>
+        <p className="py-2 text-gray-500 lg:text-lg sm:text-base mobile:text-sm">
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+      </div>
+      <div className="lg:pb-32 sm:pb-28 mobile:pb-20">
+        <ul className="flex flex-col items-center justify-center lg:px-10 lg:flex-row lg:items-start">
+          <li className="  relative flex flex-col items-center lg:items-start max-w-[21.875rem]  px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md">
+            <div className="p-6   absolute bg-[#3b3054]  top-[-2rem] flex items-center justify-center  rounded-full ">
+              <img src={recognation} alt="" />
             </div>
-            <div className='lg:pb-32 sm:pb-28 mobile:pb-20'>
-                <ul className='flex flex-col items-center justify-center lg:px-10 lg:flex-row lg:items-start'>
-                    <li className='  relative flex flex-col items-center lg:items-start max-w-[21.875rem]  px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md' >
-
-                        <div className='p-6   absolute bg-[#3b3054]  top-[-2rem] flex items-center justify-center  rounded-full ' >
-                            <img src={recognation} alt="" />
-                        </div>
-                        <h2 className='text-xl font-bold text-black'>Brand Recognition</h2>
-                        <p className='mt-5 leading-6 text-center lg:leading-relaxed lg:text-left' >Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content.</p>
-                    </li>
-                    <li className='   w-2 lg:w-[1.875rem] h-14 lg:h-2  lg:mt-[9.5rem] bg-[#2acfcf]'></li>
-                    <li className=' relative flex flex-col items-center lg:items-start max-w-[21.875rem] lg:mt-8 px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md'>
-                        <div className='  absolute bg-[#3b3054]  top-[-2rem] flex items-center justify-center p-6 rounded-full' >
-                            <img src={record} alt="" />
-                        </div>
-                        <h2 className='text-xl font-bold text-black' >Detailed Records</h2>
-                        <p className='mt-5 leading-6 text-center lg:leading-relaxed lg:text-left'>Gain insights into who is clicking your links. Knowing when and where people with you content helps inform better decisions.</p>
-                    </li>
-                    <li className='   w-2 lg:w-[1.875rem] h-14 lg:h-2  lg:mt-[9.5rem] bg-[#2acfcf]'></li>
-                    <li className=' lg:mt-16 relative flex flex-col items-center lg:items-start max-w-[21.875rem]  px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md'>
-                        <div className='  absolute p-6 bg-[#3b3054]  top-[-2rem] flex items-center justify-center w-[5.5rem] h-[5.5rem] rounded-full'  >
-                            <img src={customize} alt="" />
-                        </div>
-                        <h2 className='text-xl font-bold text-black' >Fully Customizable</h2>
-                        <p className='mt-5 leading-6 text-center lg:leading-relaxed lg:text-left'>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
-                    </li>
-                </ul>
+            <h2 className="text-xl font-bold text-black">Brand Recognition</h2>
+            <p className="mt-5 leading-6 text-center lg:leading-relaxed lg:text-left">
+              Boost your brand recognition with each click. Generic links don't
+              mean a thing. Branded links help instill confidence in your
+              content.
+            </p>
+          </li>
+          <li className="   w-2 lg:w-[1.875rem] h-14 lg:h-2  lg:mt-[9.5rem] bg-[#2acfcf]"></li>
+          <li className=" relative flex flex-col items-center lg:items-start max-w-[21.875rem] lg:mt-8 px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md">
+            <div className="  absolute bg-[#3b3054]  top-[-2rem] flex items-center justify-center p-6 rounded-full">
+              <img src={record} alt="" />
             </div>
-            <div className='boost-banner gap-5 bg-[#3b3054] py-14 flex flex-col items-center justify-center'>
-                <h2 className='font-bold text-white lg:text-5xl md:text-4xl sm:text-3xl mobile:text-lg ' >Boost your links today</h2>
-                <button className='text-white font-bold lg:text-lg md:text-bse sm:text-base mobile:text-sm bg-[#2acfcf] py-[10px] px-12 rounded-[20px] '>Get Startr </button>
+            <h2 className="text-xl font-bold text-black">Detailed Records</h2>
+            <p className="mt-5 leading-6 text-center lg:leading-relaxed lg:text-left">
+              Gain insights into who is clicking your links. Knowing when and
+              where people with you content helps inform better decisions.
+            </p>
+          </li>
+          <li className="   w-2 lg:w-[1.875rem] h-14 lg:h-2  lg:mt-[9.5rem] bg-[#2acfcf]"></li>
+          <li className=" lg:mt-16 relative flex flex-col items-center lg:items-start max-w-[21.875rem]  px-9 lg:px-8 pb-10 lg:pb-9 pt-20 bg-white rounded-md">
+            <div className="  absolute p-6 bg-[#3b3054]  top-[-2rem] flex items-center justify-center w-[5.5rem] h-[5.5rem] rounded-full">
+              <img src={customize} alt="" />
             </div>
-        </div>
-    )
+            <h2 className="text-xl font-bold text-black">Fully Customizable</h2>
+            <p className="mt-5 leading-6 text-center lg:leading-relaxed lg:text-left">
+              Improve brand awareness and content discoverability through
+              customizable links, supercharging audience engagement.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="boost-banner gap-5 bg-[#3b3054] py-14 flex flex-col items-center justify-center">
+        <h2 className="font-bold text-white lg:text-5xl md:text-4xl sm:text-3xl mobile:text-lg ">
+          Boost your links today
+        </h2>
+        <button className="text-white font-bold lg:text-lg md:text-bse sm:text-base mobile:text-sm bg-[#2acfcf] py-[10px] px-12 rounded-[20px] ">
+          Get Startr{" "}
+        </button>
+      </div>
+    </div>
+  );
 }
--- my-project/src/layoutes/HeaderLayout.jsx
+++ my-project/src/layoutes/HeaderLayout.jsx
@@ -1,47 +1,59 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import { navbarData } from '../helpers/NavbarData';
-import { RiMenuLine } from "react-icons/ri";
-import logo from '../assets/image/logo.svg'
-import { Link } from 'react-router-dom'
+import { navbarData } from "../helpers/NavbarData";
+import { VscMenu } from "react-icons/vsc";
+import logo from "../assets/image/logo.svg";
+import { Link } from "react-router-dom";
 
 export default function HeaderLayout() {
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    const showNav = () => {
-        setToggle(!toggle);
-    };
+  const showNav = () => {
+    setToggle(!toggle);
+  };
 
-    return (
-        <div className='flex flex-row   lg:items-center sm:pt-7 mobile:pt-7 md:pt-7  justify-around  container   ' >
-            <div className='logo-img'>
-                <img className='logo' src={logo} alt="" />
-            </div>
-            <div className=' flex md:flex-col  lg:flex-row sm:flex-col mobile:flex-col'>
-                <button className="flex justify-end rounded  lg:hidden"
-                    onClick={showNav} >
-                    <RiMenuLine />
-                </button>
-                <ul className={`${toggle ? " flex" : " hidden"
-                        } flex-col sm:bg-[#3b3054] lg:flex  sm:mt-4 sm:px-24 sm:py-4  mobile:bg-[#3b3054] md:bg-[#3b3054] md:py-8 md:px-28  md:gap-8 lg:mt-0  lg:p-0   lg:bg-white sm:rounded-t-md  lg:flex-row mobile:rounded-t-md md:text-white sm:text-white justify-center items-center `}>
-                    {navbarData.map((link, index) => {
-                        return (
-                            <li key={index} className={link.cname}>
-                                <Link className="hover:text-sky-500"
-                                    to={link.href}
-                                    onClick={showNav}
-                                >
-                                    {link.title}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <div className={`${toggle ? " flex" : " hidden"} lg:flex lg:ms-[20%] sm:bg-[#3b3054] md:bg-[#3b3054] md:px-28 md:py-4 mobile:rounded-b-md mobile:p-10 mobile:flex-col mobile:bg-[#3b3054] sm:rounded-b-md md:rounded-b-md    lg:bg-white  sm: mx-auto md:mx-auto  gap-2 lg:flex-row md:flex-col  sm:flex-col  p-2 sm:m-0 md:m-0 lg:mt-4 `}>
-                    <button className='font-mono font-bold text-gray-400 '>Login</button>
-                    <button className='bg-[#2acfcf] text-white  rounded-[30px] py-2 px-5 '>SingUp</button>
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div className="container flex flex-row justify-around lg:items-center sm:pt-7 mobile:pt-7 md:pt-7 ">
+      <div className="logo-img">
+        <img className="logo" src={logo} alt="" />
+      </div>
+      <div className="flex md:flex-col lg:flex-row sm:flex-col mobile:flex-col">
+        <button
+          className="flex justify-end rounded lg:hidden"
+          onClick={showNav}
+        >
+          <VscMenu />
+        </button>
+        <ul
+          className={`${
+            toggle ? " flex" : " hidden"
+          } flex-col sm:bg-[#3b3054] lg:flex  sm:mt-4 sm:px-24 sm:py-4  mobile:bg-[#3b3054] md:bg-[#3b3054] md:py-8 md:px-28  md:gap-8 lg:mt-0  lg:p-0   lg:bg-white sm:rounded-t-md  lg:flex-row mobile:rounded-t-md md:text-white sm:text-white justify-center items-center `}
+        >
+          {navbarData.map((link, index) => {
+            return (
+              <li key={index} className={link.cname}>
+                <Link
+                  className="hover:text-sky-500"
+                  to={link.href}
+                  onClick={showNav}
+                >
+                  {link.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div
+          className={`${
+            toggle ? " flex" : " hidden"
+          } lg:flex lg:ms-[20%] sm:bg-[#3b3054] md:bg-[#3b3054] md:px-28 md:py-4 mobile:rounded-b-md mobile:p-10 mobile:flex-col mobile:bg-[#3b3054] sm:rounded-b-md md:rounded-b-md    lg:bg-white  sm: mx-auto md:mx-auto  gap-2 lg:flex-row md:flex-col  sm:flex-col  p-2 sm:m-0 md:m-0 lg:mt-4 `}
+        >
+          <button className="font-mono font-bold text-gray-400 ">Login</button>
+          <button className="bg-[#2acfcf] text-white  rounded-[30px] py-2 px-5 ">
+            SingUp
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
